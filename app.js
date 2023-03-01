3c073e4b1797bc2344d61103c7803e9aea13c311
JeonGAMJA <indian_potato@naver.com>
2023-03-01 20:15:35 +0900

    Learn : Function JS

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-const player = {
-    name: "nico",
-    points: 10,
-    fat: true
-};
-console.log(player);
-player.points = player.points +15;
-console.log(player);
+function sayHello(nameOfPerson, age){
+    console.log("Hello my name is " + nameOfPerson + " and my age is " + age);
+}
 
+sayHello("Gamja", 10);
