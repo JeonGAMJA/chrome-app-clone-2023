6ad007ce7151fd4d2c5d75ef53de53b0742131a8
JeonGAMJA <indian_potato@naver.com>
2023-03-01 18:38:25 +0900

    array JS

--- app.js
+++ app.js
@@ -1,14 +1,2 @@
-const a = 5;
-const b = 2;
-let myName = "Gamja";
-//const = cnostant(상수), 값이 바뀔 수 없음
-//let, 생성할 때 사용 생성 후 값을 바굴 수 있음, 재선언x 재할당o
-//var 어디서든 변경 가능 재선언 o 재할당o 실수로 값을 업데이트해도 알아차릴수 없다는 단점이 있다.
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello"+myName);
-
-myName = "JeonGamja";
-
-console.log("your new name is "+ myName);
+const toBuy = ["potato","tomato","pizza"];
+console. log(toBuy[2]);
