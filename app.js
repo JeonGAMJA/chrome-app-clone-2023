504ddd1e4bce8e227ea0cd73fee744c5cc8128bf
JeonGAMJA <indian_potato@naver.com>
2023-03-03 21:23:02 +0900

    Learn:Calculator JS

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const player = {
-    name: "Gamja",
-    sayHello: function(otherPersonsName) {
-        console.log("hello! " + otherPersonsName + " nice to meet you!");
+const calculator = {
+    add: function (a,b){
+        console.log(a+"+"+b+"="+ (a+b));
     },
-};
+    minus: function (a,b){
+        console. log(a+"-"+b+"="+ (a-b));
+    },
+    divide: function(a,b){
+        console. log(a+"/"+b+"="+ (a/b));
+    },
+    multi: function(a,b){
+        console. log(a+"*"+b+"="+ (a*b));
+    },
+    power: function(a,b){
+        console. log(a+"**"+b+"="+ (a**b));
+    }
+    };
 
-console.log(player.name);
-player.sayHello("Lynn");
-player.sayHello("Gamja");
+calculator.add(1,2);
+calculator.minus(1,2);
+calculator.divide(1,2);
+calculator.multi(1,2);
+calculator.power(1,2);
